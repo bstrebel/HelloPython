be0e0436a7cf27997ef8060f39a12572f40a2100
Bernd Strebel <b.strebel@digitec.de>
2015-11-28 05:51:11 +0100

    snapshot

--- Language/Dictionary.py
+++ Language/Dictionary.py
@@ -1,5 +1,27 @@
 __author__ = 'bst'
 
+myDict = {'key1': 'value1',
+          'key2': 'value2'}
+
+myDict.setdefault('scraper', {})
+
+for k in myDict.keys():
+    print(k)
+
+if 'key1' in myDict:
+    print("Found!")
+
+for k, v in myDict.items():
+    print(k, v)
+
+print('[myDict]')
+for d in myDict:
+    print(d)
+
+
+
+
+
 def init(e):
     _entry = e
     print _entry
